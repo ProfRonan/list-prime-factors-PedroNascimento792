97739644ddbbc2831c87ac9c95e7484d91ad53f4
PedroNascimento792 <pedrocapedrocas@gmail.com>
2023-05-27 21:11:19 -0300

    resolvido

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 """Módulo com funções."""
-
+from typing import List
 
 def is_prime(number: int) -> bool:
     """Retorna True se o número for primo e False caso contrário."""
-
-    return False
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0,5)+ 1):
+        if number % i == 0:
+            return False
+    return True    
 
 
 def list_prime_factors(number: int) -> list[int]:
     """Retorna uma lista com os fatores primos de cada número da lista fornecida."""
+    factors = []
+    divisor = 2
 
-    return []
+    while number > 1:
+        if number % divisor == 0:
+            factors.append(divisor)
+            number //= divisor
+        else:
+            divisor += 1
+
+    return factors
+
+number = 84
+result = list_prime_factors(number)
+print(result)
